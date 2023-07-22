1c3de86b9436145dcd3f79ce91f7e0c3f56fd013
Matej Bačo <matejbaco2000@gmail.com>
2023-07-22 22:22:24 +0200

    Update main.js

--- node/starter-template/src/main.js
+++ node/starter-template/src/main.js
@@ -3,8 +3,6 @@ import { Client } from 'node-appwrite';
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  if(req.url === '/title') {
-    const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
-    return res.send(`<p>Now is <b>${date}</b>. <p>`);
-  }
+  const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+  return res.send(`<p>Now is <b>${date}</b>. <p>`);
 };
